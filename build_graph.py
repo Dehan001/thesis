2173c6fc4f53c43fbdb43bcbeb3c3ed09893b5ee
Dehan001 <dehannoor3199@gmail.com>
2024-01-20 09:26:24 -0800

    second commit

--- build_graph.py
+++ build_graph.py
@@ -15,7 +15,7 @@ from scipy.spatial.distance import cosine
 if len(sys.argv) != 2:
 	sys.exit("Use: python build_graph.py <dataset>")
 
-datasets = ['Sentiment','BanFake','BengaliHateSpeech','SarcasDetection','20ng', 'R8', 'R52', 'ohsumed', 'mr','SentNOB','Emotion']
+datasets = ['BanglaErrorBin','BanglaErrorMulti','Sentiment','BanFake','BengaliHateSpeech','SarcasDetection','20ng', 'R8', 'R52', 'ohsumed', 'mr','SentNOB','Emotion']
 # build corpus
 dataset = sys.argv[1]
 
